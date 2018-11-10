6489b80cb463d824af106ce65fa5c32bc746638b
Edsel Caprice <caprice@mail.usf.edu>
2018-11-10 18:31:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,11 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(email: params[:email], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    end
+    redirect '/login'
   end
 
   get '/account' do
